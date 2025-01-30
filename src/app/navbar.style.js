5707d32877e4e16e3ca199ef3b0c18b45c014df4
aymen <aymenjani20@gmail.com>
2025-01-30 20:38:08 +0100

    30/01

---
+++ src/app/navbar.style.js
@@ -0,0 +1,53 @@
+import styled from"@emotion/styled"
+
+export const Maincontainer= styled.div`
+position : absolute ; 
+top : 10% ; 
+left : 1%; 
+min-height : 50vh ;
+width : 300px ;  
+min-width : 30wv ; 
+
+border-radius : 20px ; 
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+
+`
+export const Pager = styled.div`
+    min-height : 100vh ; 
+  background-color: #7f5a83;
+  background-image: linear-gradient(315deg, #7f5a83 0%, #0d324d 74%);
+
+
+`
+export const container  = styled.div`
+padding-left : 10px ; 
+padding-top : 20px ; 
+
+`
+export const MenuHandler = styled.div`
+display : flex ; 
+align-items : start ; 
+justify-content : space-between ; 
+flex-direction : column ; 
+gap : 20px ; 
+margin-top : 10px ; 
+padding-left : 10px ; 
+padding-top : 10px ;
+`
+export const menuitem = styled.div`
+display : flex ; 
+justify-content : center ; 
+align-items : center ; 
+gap  : 10px ; 
+`
+export const title=  styled.div`
+
+`
+export const icon  = styled.div`
+
+`
+
+export const body = styled.div`
+
+`
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,54 @@
 "use client"
-import { useLayoutEffect } from "react";
-import Loadingpage from "./loading";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-import SideNavBar from "../../components/SideNavBar";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { CgProfile } from "react-icons/cg";
+import { FaRegComments } from "react-icons/fa";
+import { BiMessageSquareDots } from "react-icons/bi";
+import { useState } from "react";
+import * as s from "./navbar.style"
+function My_navbar () {
+  const Menu =   [{name : "Virtual Dashboard" ,icon :FaRegComments} , {name : "Dashboard" ,icon :CgProfile}  ,{name : "Profile" ,icon :BiMessageSquareDots}  ,{name : "Comments" ,icon :CgProfile} ]
+  const Menujsx  = Menu.map((menu)=>{
+    return( <s.menuitem key = {menu.name}>
+            <menu.icon className="text-2xl text-gray-600 group-hover:text-white " />
+      <s.title>
+      {menu.name}
+      </s.title>
+     
+
+    </s.menuitem>)
+  })
+  return(
+    <s.Maincontainer  >
+         
+          <s.MenuHandler>
+          {Menujsx}
+          </s.MenuHandler>
+     
+      </s.Maincontainer>
+  )
+
+}
 export default function Home() {
-/* useGSAP(()=>{
-  let t1= gsap.timeline();
-  t1.to(".box",{
-    scale : 0 , 
-    y: 60 , 
-    rotate : 400,
-    duration :1 , 
-    repeat : 1 , 
-    yoyo : true , 
-    delay : 0.5  ,
-    stagger:{
-      amount : 1.5,
-      from : "start" , 
-     // axis : "x" 
-     grid : [3,3] , 
-
-    }
-
-  })
-  t1.to(".container" ,{
-    rotate : "-405deg" , 
-    scale : 0, 
-    duration : 1, 
-
-  })
-  t1.to(".wrapper" ,{
-    opacity : 0,
-    display : "none" , 
-  })
-}) */
+const [Toggle,setToggle] = useState(true)
   return (
     <> 
-    <div>
-    <SideNavBar/>
-    </div>
+    <s.Pager  >
+      <s.container >
+      <Disclosure as="nav">
+      
+      <DisclosureButton onClick={()=> setToggle(!Toggle)} >
+              <GiHamburgerMenu className="block  h-6 w-6" />
+            </DisclosureButton>
+      </Disclosure>
+      {Toggle && <My_navbar /> }
+     
+      </s.container>
+      <s.body>
+        
+      </s.body>
+
+    </s.Pager>
     
     </>
   );
